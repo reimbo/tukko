91388f6fe957a232f45292dd7672ef28ee6daa8b
Ab6225 <ab6225@student.jamk.fi>
2023-05-26 13:04:36 +0300

    Add Geoman

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,8 +1,10 @@
-import { MapContainer, Popup, Marker, TileLayer } from "react-leaflet";
+import { useMap, MapContainer, TileLayer } from "react-leaflet";
 import './leaflet.css'
 import './root.css'
+import '@geoman-io/leaflet-geoman-free';
+import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
 
-function MapPlaceholder() {
+function MapPlaceholder(): JSX.Element {
   return (
     <p>
       Traffic Visualizer
@@ -11,19 +13,37 @@ function MapPlaceholder() {
   )
 }
 
+function Geoman() {
+  const map = useMap();
+  map.pm.addControls({
+    drawMarker: false,
+    rotateMode: false,
+    customControls: false,
+    drawText: false,
+    drawCircleMarker: false,
+    drawPolyline: false,
+    cutPolygon: false
+  });
+  return null;
+}
+
 export default function root(): JSX.Element {
   return(
     <MapContainer
-      center={[62.24144, 25.758846]}
+      center={[65.24144, 25.758846]}
       maxBounds={[[70.182772,18.506675],[59.712756,32.559953]]}
       maxBoundsViscosity={0.9}
       zoomDelta={0}
-      zoom={7}
+      zoom={6}
       placeholder={<MapPlaceholder />}>
+
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-    </MapContainer>  
+
+      <Geoman />
+
+    </MapContainer>
   )
 }
