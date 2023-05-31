3fb0e3921d166ad82e4f4ec11c9461cca5e3504e
AA4157 <AA4157@student.jamk.fi>
2023-05-31 12:56:09 +0300

    something

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -64,9 +64,6 @@ const orangeIcon = new L.Icon({
 
 
 
-
-
-
 export default function Root(): JSX.Element {
   // update this variable to change the display station
   const displayStation = 136;
@@ -84,15 +81,6 @@ export default function Root(): JSX.Element {
   }
 
   const firstLocation = stationData.length > 0 ? stationData[0] : null;
-
-  const greenIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  });
 
   return(
 
