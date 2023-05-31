5a8528773d5885fe602eb5682e09b3067c1faab2
AA4157 <AA4157@student.jamk.fi>
2023-05-31 10:19:06 +0300

    Added colored markers and hover

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,10 @@
-import { useMap, MapContainer, TileLayer, Pane, Marker, Popup, LayersControl, LayerGroup } from "react-leaflet";
+import { useMap, MapContainer, TileLayer, Marker, Popup, LayersControl, LayerGroup } from "react-leaflet";
+import L from "leaflet";
 import './leaflet.css'
 import './root.css'
 import '@geoman-io/leaflet-geoman-free';
 import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import coordsData from "./data/coords.json"
 
 function MapPlaceholder(): JSX.Element {
@@ -29,12 +30,50 @@ function Geoman() {
   return null;
 }
 
+{/* https://github.com/pointhi/leaflet-color-markers */}
+
+const greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+const redIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+const orangeIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+
+
 
 
 
 export default function root(): JSX.Element {
 
-
+  const greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
 
   return(
 
@@ -73,7 +112,10 @@ export default function root(): JSX.Element {
             {coordsData.map(coords => (
               <Marker 
               key = {coords.properties.id}
-              position={[coords.geometry.coordinates[1], coords.geometry.coordinates[0]]}>
+              position={[coords.geometry.coordinates[1], coords.geometry.coordinates[0]]}
+              eventHandlers={{
+                mouseover: (event) => event.target.openPopup(),
+              }}>
                 <Popup>
                   {coords.properties.tasks}
                 </Popup>
@@ -81,6 +123,80 @@ export default function root(): JSX.Element {
             ))}
           </LayerGroup>
         </LayersControl.Overlay>
+
+        <LayersControl.Overlay name="Show markers 2">
+          <LayerGroup>
+            {coordsData.map(coords => (
+              <Marker 
+              key = {coords.properties.id}
+              position={[coords.geometry.coordinates[1], coords.geometry.coordinates[0]]}
+              icon={greenIcon}
+              eventHandlers={{
+                mouseover: (event) => event.target.openPopup(),
+              }}>
+                <Popup>
+                  {coords.properties.tasks}
+                </Popup>
+              </Marker>
+            ))}
+          </LayerGroup>
+        </LayersControl.Overlay>
+
+        <LayersControl.Overlay name="Show markers 3">
+          <LayerGroup>
+            {coordsData.map(coords => (
+              <Marker 
+              key = {coords.properties.id}
+              position={[coords.geometry.coordinates[1], coords.geometry.coordinates[0]]}
+              icon={redIcon}
+              eventHandlers={{
+                mouseover: (event) => event.target.openPopup(),
+              }}>
+                <Popup>
+                  {coords.properties.tasks}
+                </Popup>
+              </Marker>
+            ))}
+          </LayerGroup>
+        </LayersControl.Overlay>
+
+        <LayersControl.Overlay name="Show markers 4">
+          <LayerGroup>
+          {coordsData.map(coords => (
+              <Marker 
+              key = {coords.properties.id}
+              position={[coords.geometry.coordinates[1], coords.geometry.coordinates[0]]}
+              icon={orangeIcon}
+              eventHandlers={{
+                mouseover: (event) => event.target.openPopup(),
+              }}>
+                <Popup>
+                  {coords.properties.tasks}
+                </Popup>
+              </Marker>
+            ))}
+          </LayerGroup>
+        </LayersControl.Overlay>
+
+        <LayersControl.Overlay name="Show markers 5">
+          <LayerGroup>
+            
+          </LayerGroup>
+        </LayersControl.Overlay>
+
+        <LayersControl.Overlay name="Show markers 6">
+          <LayerGroup>
+            
+          </LayerGroup>
+        </LayersControl.Overlay>
+
+        <LayersControl.Overlay name="Show markers 7">
+          <LayerGroup>
+            
+          </LayerGroup>
+        </LayersControl.Overlay>
+            
+      
       </LayersControl>
 
 
