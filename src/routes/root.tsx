b18fd074fc5bc9384fcc28543f31539ccae84758
Ab6225 <ab6225@student.jamk.fi>
2023-06-05 11:00:48 +0300

    Fix images to be local

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -56,20 +56,28 @@ function Geoman() {
   return null;
 }
 
-{/* https://github.com/pointhi/leaflet-color-markers */}
+{/* images/markers */}
+const blueIcon = new L.Icon({
+   iconUrl: 'images/marker-icon-2x-blue.png',
+   shadowUrl: 'images/marker-shadow.png',
+   iconSize: [25, 41],
+   iconAnchor: [12, 41],
+   popupAnchor: [1, -34],
+   shadowSize: [41, 41]
+});
 
-// const greenIcon = new L.Icon({
-//   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-//   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-//   iconSize: [25, 41],
-//   iconAnchor: [12, 41],
-//   popupAnchor: [1, -34],
-//   shadowSize: [41, 41]
-// });
+const greenIcon = new L.Icon({
+   iconUrl: 'images/marker-icon-2x-green.png',
+   shadowUrl: 'images/marker-shadow.png',
+   iconSize: [25, 41],
+   iconAnchor: [12, 41],
+   popupAnchor: [1, -34],
+   shadowSize: [41, 41]
+});
 
 const redIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconUrl: 'images/marker-icon-2x-red.png',
+  shadowUrl: 'images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
@@ -77,8 +85,8 @@ const redIcon = new L.Icon({
 });
 
 const orangeIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconUrl: 'images/marker-icon-2x-orange.png',
+  shadowUrl: 'images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
