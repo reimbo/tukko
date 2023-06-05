a506363cb9e2ca21f941148f99185f01559c95f6
AA7776 <aa7776@student.jamk.fi>
2023-06-05 14:43:34 +0300

    some changes

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -5,7 +5,7 @@ import './root.css'
 import '@geoman-io/leaflet-geoman-free';
 import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
 import { Fragment } from "react";
-import coordsData from "./data/coords.json";
+import coordsData from "./data/coords.json"
 import { stationLocation, stationName,stationList, fetchStations } from '../api/getStations';
 import { sensorList,FetchSensors } from '../api/getSensors';
 import stationData from '../routes/data/stationData.json';
@@ -38,28 +38,20 @@ function Geoman() {
   return null;
 }
 
-{/* images/markers */}
-const blueIcon = new L.Icon({
-   iconUrl: 'images/marker-icon-2x-blue.png',
-   shadowUrl: 'images/marker-shadow.png',
-   iconSize: [25, 41],
-   iconAnchor: [12, 41],
-   popupAnchor: [1, -34],
-   shadowSize: [41, 41]
-});
+{/* https://github.com/pointhi/leaflet-color-markers */}
 
 const greenIcon = new L.Icon({
-   iconUrl: 'images/marker-icon-2x-green.png',
-   shadowUrl: 'images/marker-shadow.png',
-   iconSize: [25, 41],
-   iconAnchor: [12, 41],
-   popupAnchor: [1, -34],
-   shadowSize: [41, 41]
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
 });
 
 const redIcon = new L.Icon({
-  iconUrl: 'images/marker-icon-2x-red.png',
-  shadowUrl: 'images/marker-shadow.png',
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
@@ -67,14 +59,13 @@ const redIcon = new L.Icon({
 });
 
 const orangeIcon = new L.Icon({
-  iconUrl: 'images/marker-icon-2x-orange.png',
-  shadowUrl: 'images/marker-shadow.png',
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41]
 });
-
 
 
 
@@ -130,6 +121,14 @@ export default function Root(): JSX.Element {
   // initialize a default first station location to check if data is available
   const firstLocation = stationLocation.length > 0 ? stationLocation[0] : null;
 
+  const greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
 
 
   // The combined data is passed to the MapContainer as props
@@ -147,17 +146,11 @@ export default function Root(): JSX.Element {
     )
   };
 
-
   return(
 
     <Fragment>
 
     <p className="overlay-title">Traffic Visualizer</p>
-    <div className="logosContainer">
-      <a href="https://www.wimmalab.org/fi" target="_blank"><img className="wimmaLabLogo" src={wimmaLabLogo} alt="WIMMA Lab Logo"/></a>
-      <a href="https://wimma-lab-2023.pages.labranet.jamk.fi/iotitude/core-traffic-visualizer/" target="_blank"><img className="iotitudeLogo" src={iotitudeLogo} alt="IoTitude Logo"/></a>
-    </div>
-
     <div className="logosContainer">
       <a href="https://www.wimmalab.org/fi" target="_blank"><img className="wimmaLabLogo" src={wimmaLabLogo} alt="WIMMA Lab Logo"/></a>
       <a href="https://wimma-lab-2023.pages.labranet.jamk.fi/iotitude/core-traffic-visualizer/" target="_blank"><img className="iotitudeLogo" src={iotitudeLogo} alt="IoTitude Logo"/></a>
@@ -171,15 +164,12 @@ export default function Root(): JSX.Element {
       <label >Hide Markers</label><br/>   
     </div> */}
 
-    
-
       <MapContainer
-        center={[65.24144, 25.758846]}
+        center={firstLocation ? [firstLocation.latitude, firstLocation.longitude] : [65.24144, 25.758846]}
         maxBounds={[[70.182772, 18.506675], [59.712756, 32.559953]]}
         maxBoundsViscosity={0.9}
         zoomDelta={0}
-        zoom={6}
-        minZoom={5}
+        zoom={12}
         placeholder={<MapPlaceholder />}
       >
         <TileLayer
@@ -227,18 +217,19 @@ export default function Root(): JSX.Element {
 
         <LayersControl.Overlay name="Show markers 2">
           <LayerGroup>
-              {coordsData.map(coords => (
-                <Marker 
-                key = {coords.properties.id}
-                position={[coords.geometry.coordinates[1], coords.geometry.coordinates[0]]}
-                eventHandlers={{
-                  mouseover: (event) => event.target.openPopup(),
-                }}>
-                  <Popup>
-                    {coords.properties.tasks}
-                  </Popup>
-                </Marker>
-              ))}
+            {coordsData.map(coords => (
+              <Marker 
+              key = {coords.properties.id}
+              position={[coords.geometry.coordinates[1], coords.geometry.coordinates[0]]}
+              icon={greenIcon}
+              eventHandlers={{
+                mouseover: (event) => event.target.openPopup(),
+              }}>
+                <Popup>
+                  {coords.properties.tasks}
+                </Popup>
+              </Marker>
+            ))}
           </LayerGroup>
         </LayersControl.Overlay>
 
@@ -269,45 +260,44 @@ export default function Root(): JSX.Element {
         </LayersControl.Overlay>
 
         <LayersControl.Overlay name="Stations">
-            <LayerGroup>
-            {combinedData.stationList.map((stationId, index) => (
+          <LayerGroup>
+          {stationLocation.map(stations => (
               <Marker 
-                key={stationId}
-                position={[
-                  combinedData.stationLocation[index].latitude,
-                  combinedData.stationLocation[index].longitude
-                ]}
-                eventHandlers={{
-                  mouseover: (event) => event.target.openPopup()
-                }}
-                icon={orangeIcon}
-              >
+              position={[stations.latitude, stations.longitude ]}
+              icon={orangeIcon}
+              eventHandlers={{
+                mouseover: (event) => event.target.openPopup(),
+              }}>
                 <Popup>
-                  Station name: {combinedData.stationName[index]} <br/>
-                  Station id: {stationId} <br/>
+                  Station name:<br/>
+                  Station id: <br/>
+                  Unit: <br/>
+                  Value:
                 </Popup>
               </Marker>
             ))}
-            </LayerGroup>
+          </LayerGroup>
         </LayersControl.Overlay>
         <LayersControl.Overlay name="Show markers 5">
-            <LayerGroup>
+          <LayerGroup>
             
-            </LayerGroup>
-          </LayersControl.Overlay>
+          </LayerGroup>
+        </LayersControl.Overlay>
 
         <LayersControl.Overlay name="Show markers 6">
-            <LayerGroup>
-              
-            </LayerGroup>
+          <LayerGroup>
+            
+          </LayerGroup>
         </LayersControl.Overlay>
 
         <LayersControl.Overlay name="Show markers 7">
-            <LayerGroup>
-              
-            </LayerGroup>
+          <LayerGroup>
+            
+          </LayerGroup>
         </LayersControl.Overlay>
-    </LayersControl>
+            
+      
+      </LayersControl>
 
 
     </MapContainer>
