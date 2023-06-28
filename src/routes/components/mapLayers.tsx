1ee5ad183d86cbfd52f0a7efe47c36f0ce3253ae
AA4157 <AA4157@student.jamk.fi>
2023-06-28 12:42:57 +0300

    Tooltip visualization done

--- src/routes/components/mapLayers.tsx
+++ src/routes/components/mapLayers.tsx
@@ -1,12 +1,19 @@
-import { LayersControl, Marker, LayerGroup, Popup } from "react-leaflet";
+import { LayersControl, Marker, LayerGroup, Popup, Tooltip} from "react-leaflet";
 import MarkerClusterGroup from 'react-leaflet-cluster';
+import styles from "./css/mapLayers.module.css"
+import "./css/Tooltip.css"
+import carIcon from "../../assets/tooltipIcons/car-side-solid.svg";
+import compassIcon from "../../assets/tooltipIcons/compass-solid.svg"; // placeholder icon
+
+// Components
 import { createMarker } from "./Icons";
 import { Station } from "../../interfaces/sensorInterfaces";
-import styles from "./css/mapLayers.module.css"
+
 
 export function MapLayers({ data }: { data: Station[] | null }): JSX.Element {
   const MarkerList = data?.map(
     (station) => {
+      console.log(station)
       if (station.sensorValues.length > 0) return (
         <Marker
           pmIgnore
@@ -27,6 +34,27 @@ export function MapLayers({ data }: { data: Station[] | null }): JSX.Element {
             })}
             </ul>
           </Popup>
+          <Tooltip>
+            <h1 className="place-name">{station.name}</h1>
+                  <div className="grid-container">
+                      <div className="grid-item grid-top-left">
+                          <img src={carIcon} alt="Car icon" className="tooltip-icon-left tooltip-icon-reverse" />
+                          <div className="tooltop-div tooltip-div-car">XXXX <br/> cars/h</div>
+                      </div>
+                      <div className="grid-item grid-top-right">
+                          <img src={compassIcon} alt="Car icon" className="tooltip-icon-right tooltip-icon-reverse" />
+                          <div className="tooltop-div">Towards <br/> direction1</div>
+                      </div>
+                      <div className="grid-item grid-bottom-left">
+                          <img src={carIcon} alt="Car icon" className="tooltip-icon-left" />
+                          <div className="tooltop-div tooltip-div-car">XXX <br/> cars/h</div>
+                      </div>
+                      <div className="grid-item grid-bottom-right">
+                          <img src={compassIcon} alt="Car icon" className="tooltip-icon-right" />
+                          <div className="tooltop-div">Towards <br/> direction2</div>
+                      </div>
+                  </div>
+          </Tooltip>
           </Marker>
       )
     }
