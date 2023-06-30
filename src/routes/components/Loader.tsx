fcd453ee5a52dbaed33ea9eea3e28add3a4e2865
AA4157 <aa4157@student.jamk.fi>
2023-06-30 14:54:35 +0300

    Merge branch 'main' of...

--- src/routes/components/Loader.tsx
+++ src/routes/components/Loader.tsx
@@ -1,12 +1,12 @@
 import "./css/Loader.css"
 
-// this component returns loading screen for travis
+// this component returns loading screen for tukko
 
 export const Loader = () => {
 return (
     <div className="loaderContainer">
         <div className="center">
-            <p className="loaderText">Traffic Visualizer </p>
+            <p className="loaderText">Tukko </p>
             <div className="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
             <p className="loaderText">Now Loading... </p>
         </div>
