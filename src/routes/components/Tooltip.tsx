b28b865703766943a52cfbb83640d394c480e781
Ab6225 <ab6225@student.jamk.fi>
2023-06-30 15:48:22 +0300

    Change direction on click of appropriate section

--- src/routes/components/Tooltip.tsx
+++ src/routes/components/Tooltip.tsx
@@ -3,30 +3,34 @@ import { Station } from "../../interfaces/sensorInterfaces";
 import carIcon from "../../assets/tooltipIcons/car-side-solid.svg";
 import compassIcon from "../../assets/tooltipIcons/compass-solid.svg"; // placeholder icon
 import "./css/Tooltip.css";
+import DirectionPopup from "./Popup";
+import { Tooltip as TT } from 'leaflet';
 
 export default function StationTooltip({station}: {station: Station}): JSX.Element {
+  const [direction, setDirection] = useState(1)
   return (
     <Tooltip>
       <h1 className="place-name">{station.name}</h1>
-            <a>Open dir1</a>
-            <div className="grid-container">
-                <div className="grid-item grid-top-left">
-                    <img src={carIcon} alt="Car icon" className="tooltip-icon-left tooltip-icon-reverse" />
-                    <div className="tooltip-div tooltip-div-car">XXXX <br/> cars/h</div>
-                </div>
-                <div className="grid-item grid-top-right">
-                    <img src={compassIcon} alt="Car icon" className="tooltip-icon-right tooltip-icon-reverse" />
-                    <div className="tooltip-div">Towards <br/> direction1</div>
-                </div>
-                <div className="grid-item grid-bottom-left">
-                    <img src={carIcon} alt="Car icon" className="tooltip-icon-left" />
-                    <div className="tooltip-div tooltip-div-car">XXX <br/> cars/h</div>
-                </div>
-                <div className="grid-item grid-bottom-right">
-                    <img src={compassIcon} alt="Car icon" className="tooltip-icon-right" />
-                    <div className="tooltip-div">Towards <br/> direction2</div>
-                </div>
-            </div>
-            <a>Open dir2</a>
+      <div onClick={() => setDirection(1)} className='grid-container'>
+        <div className="grid-item grid-top-left">
+          <img src={carIcon} alt="Car icon" className="tooltip-icon-left tooltip-icon-reverse" />
+          <div className="tooltip-div tooltip-div-car">XXXX <br/> cars/h</div>
+        </div>
+        <div className="grid-item grid-top-right">
+          <img src={compassIcon} alt="Car icon" className="tooltip-icon-right tooltip-icon-reverse" />
+          <div className="tooltip-div">Towards <br/> direction1</div>
+        </div>
+      </div>
+      <div onClick={() => setDirection(2)} className='grid-container'>
+        <div className="grid-item grid-bottom-left">
+          <img src={carIcon} alt="Car icon" className="tooltip-icon-left" />
+          <div className="tooltip-div tooltip-div-car">XXX <br/> cars/h</div>
+        </div>
+        <div className="grid-item grid-bottom-right">
+          <img src={compassIcon} alt="Car icon" className="tooltip-icon-right" />
+          <div className="tooltip-div">Towards <br/> direction2</div>
+        </div>
+      </div>
+      <DirectionPopup station={station} direction={direction}/>
     </Tooltip>
   )}
