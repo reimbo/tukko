c2604b6de926d4060ee24610e33b5148c236fa19
AA4157 <AA4157@student.jamk.fi>
2023-07-13 10:57:04 +0300

    Hover effect added to tooltip

--- src/routes/components/Tooltip.tsx
+++ src/routes/components/Tooltip.tsx
@@ -36,23 +36,25 @@ export default function StationTooltip({station, marker}: {station: Station, mar
     >
       <h1 className={styles["place-name"]}>{station.names[(i18n.language as keyof Station['names'])]}</h1>
       <div  className={styles['grid-container']}>
-        <div onClick={() => setDirection(1)} className={`${styles["grid-item"]} ${styles["grid-top-left"]}`}>
-          <img src={compassIcon} alt="Car icon" className={`${styles["tooltip-icon"]}`}/>
-          <div className={styles['tooltip-div']}>{t('direction')}<br/>{station.direction1Municipality}</div>
+        <div className={styles["grid-column"]}>
+          <div onClick={() => setDirection(1)} className={`${styles["grid-item"]} ${styles["grid-top-left"]}`}>
+            <img src={compassIcon} alt="Car icon" className={`${styles["tooltip-icon"]}`}/>
+            <div className={styles['tooltip-div']}>{t('direction')}<br/>{station.direction1Municipality}</div>
+          </div>
+          <div onClick={() => setDirection(1)} className={`${styles["grid-item"]} ${styles["grid-bottom-left"]}`}>
+            <img src={carIcon} alt="Car icon" className={styles["tooltip-icon-car"]} />
+            <div className={`${styles["tooltip-div"]} ${styles["tooltip-div-car"]}`}>{station.sensors?.find(e => e.id == 5116)?.value}<br/>{t('unit')}</div>
+          </div>
         </div>
-        <div onClick={() => setDirection(2)} className={`${styles["grid-item"]} ${styles["grid-top-right"]}`}>
-          <img src={compassIcon} alt="Car icon" className={styles["tooltip-icon"]} />
-          <div className={styles['tooltip-div']}>{t('direction')}<br/>{station.direction2Municipality}</div>
-        </div>
-      </div>
-      <div className={styles['grid-container']}>
-        <div onClick={() => setDirection(1)} className={`${styles["grid-item"]} ${styles["grid-bottom-left"]}`}>
-          <img src={carIcon} alt="Car icon" className={styles["tooltip-icon-car"]} />
-          <div className={`${styles["tooltip-div"]} ${styles["tooltip-div-car"]}`}>{station.sensors?.find(e => e.id == 5116)?.value}<br/>{t('unit')}</div>
-        </div>
-        <div onClick={() => setDirection(2)} className={`${styles["grid-item"]} ${styles["grid-bottom-right"]}`}>
-          <img src={carIcon} alt="Car icon" className={styles["tooltip-icon-car"]} />
-          <div className={`${styles["tooltip-div"]} ${styles["tooltip-div-car"]}`}>{station.sensors?.find(e => e.id == 5119)?.value}<br/>{t('unit')}</div>
+        <div className={styles["grid-column"]}>
+          <div onClick={() => setDirection(2)} className={`${styles["grid-item"]} ${styles["grid-top-right"]}`}>
+            <img src={compassIcon} alt="Car icon" className={styles["tooltip-icon"]} />
+            <div className={styles['tooltip-div']}>{t('direction')}<br/>{station.direction2Municipality}</div>
+          </div>
+          <div onClick={() => setDirection(2)} className={`${styles["grid-item"]} ${styles["grid-bottom-right"]}`}>
+            <img src={carIcon} alt="Car icon" className={styles["tooltip-icon-car"]} />
+            <div className={`${styles["tooltip-div"]} ${styles["tooltip-div-car"]}`}>{station.sensors?.find(e => e.id == 5119)?.value}<br/>{t('unit')}</div>
+          </div>
         </div>
       </div>
       <DirectionPopup station={station} direction={direction}/>
