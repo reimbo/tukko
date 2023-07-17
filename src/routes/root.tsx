fd5e759ec2e7c95cf36e9657651fb848e219fc09
Ab6225 <ab6225@student.jamk.fi>
2023-07-17 13:57:57 +0300

    Add GDPR link

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -43,7 +43,7 @@ export default function Root(): JSX.Element {
         placeholder={<MapPlaceholder />}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | <a target="_blank" href="https://wimma-lab-2023.pages.labranet.jamk.fi/iotitude/core-traffic-visualizer/80-Documents-and-reporting/gdpr-statement/">GDPR</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
